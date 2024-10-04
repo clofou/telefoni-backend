216836851f861aef5c0d010eb17318e2f2762551
Fakoro <fakoro88@gmail.com>
2024-10-04 14:56:06 +0000

    first commit

---
+++ src/main/java/org/bamappli/telefonidashboard/TelefoniDashboardApplication.java
@@ -0,0 +1,13 @@
+package org.bamappli.telefonidashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelefoniDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelefoniDashboardApplication.class, args);
+	}
+
+}
