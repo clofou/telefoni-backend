8dee34b6bea188204f64752fc1b4c95520678fef
Fakoro Traoré <fakoro@Fakoros-MacBook-Air.local>
2024-10-11 17:06:53 +0000

    il

--- src/main/java/org/bamappli/telefonibackend/Repository/AvisRepo.java
+++
@@ -1,7 +0,0 @@
-package org.bamappli.telefonibackend.Repository;
-
-import org.bamappli.telefonibackend.Entity.Avis;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AvisRepo extends JpaRepository<Long, Avis> {
-}
